da815157770ca9f942c852440eb00c353e99c819
Débora <d.saioron@hotmail.com>
2022-12-21 19:10:45 -0300

    função setClock)

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const hourHand = document.querySelector('.hand.hours');
 const minutesHand = document.querySelector('.hand.minutes');
 const secondsHand = document.querySelector('.hand.secondss');
+
+const setClock = () => {
+    const currentDate = new Date();
+
+    const secondsPercentage = currentDate.getSeconds() / 60;
+    const minutesPercentage = currentDate.getminutes() / 60;
+    const hoursPercentage = currentDate.gethours() / 60;
+}
