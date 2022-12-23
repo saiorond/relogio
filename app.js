374c79c251d6b8ca82dfc62c80793d4baf76143d
Débora <d.saioron@hotmail.com>
2022-12-22 21:56:56 -0300

    função setRotation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const hourHand = document.querySelector('.hand.hours');
+const hoursHand = document.querySelector('.hand.hours');
 const minutesHand = document.querySelector('.hand.minutes');
 const secondsHand = document.querySelector('.hand.secondss');
+
+const setRotation = (element, rotationPercentage) => {
+    element.style.setProperty("--rotation", rotationPercentage * 360);
+}
 
 const setClock = () => {
     const currentDate = new Date();
@@ -8,4 +12,9 @@ const setClock = () => {
     const secondsPercentage = currentDate.getSeconds() / 60;
     const minutesPercentage = currentDate.getminutes() / 60;
     const hoursPercentage = currentDate.gethours() / 60;
+
+    setRotation(secondsHand, secondsPercentage);
+    setRotation(minutesHand, minutesPercentage);
+    setRotation(hoursHand, hoursPercentage);
+
 }
