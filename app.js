c9aece93e6141aed842cf42198369f7cf4834f18
Débora <d.saioron@hotmail.com>
2022-12-27 17:58:24 -0300

    ajustes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const hoursHand = document.querySelector('.hand.hours');
-const minutesHand = document.querySelector('.hand.minutes');
-const secondsHand = document.querySelector('.hand.secondss');
+const hoursHand = document.querySelector(".hand.hours");
+const minutesHand = document.querySelector(".hand.minutes");
+const secondsHand = document.querySelector(".hand.seconds");
 
 const setRotation = (element, rotationPercentage) => {
     element.style.setProperty("--rotation", rotationPercentage * 360);
